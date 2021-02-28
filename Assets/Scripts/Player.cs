184ddc56902ee695ee73c4efb274ae02eab13111
ledoyen <ledoyen.loic@gmail.com>
2021-02-28 16:47:13 +0100

    Laser shooting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,16 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 10F;
     [SerializeField] float shipWingWidth = 0.2F;
     [SerializeField] float topInaccesibleScreenPortion = 0.33F; // 33 %
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float timeBetweenFires = 0.2F; // -> 5 times per second
+    [SerializeField] float laserSpeed = 10F;
 
     float minY;
     float maxY;
     float minX;
     float maxX;
+
+    float lastTimeShootingOccured = 0F;
 
     void Start()
     {
@@ -29,6 +34,22 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        Move();
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetButton("Fire1") && lastTimeShootingOccured < Time.time - timeBetweenFires)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, transform.rotation);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+            lastTimeShootingOccured = Time.time;
+        }
+    }
+
+    private void Move()
+    {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
@@ -36,6 +57,5 @@ public class Player : MonoBehaviour
         var newY = Mathf.Clamp(transform.position.y + deltaY, minY, maxY);
 
         transform.position = new Vector2(newX, newY);
-        Debug.Log(transform.position);
     }
 }
